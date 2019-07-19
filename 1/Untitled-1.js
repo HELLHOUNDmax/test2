7ec23a2893681c754a0985b5be2dc8e9b123190f
Max <belousovmaxdnepr@gmail.com>
2019-07-19 16:45:05 +0300

    first commit

---
+++ 1/Untitled-1.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var LeftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(LeftBorderWidth);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[1])
+
+//alert("Hello world");
+
+//let answer = confirm("Are you here");
+
+//console.log(answer);
+
+/*let answer = +prompt("Есть ли вам 18?", "Да");
+
+console.log(typeof(answer));
+
+console.log("arr" + " -object");
+console.log(4 + +" - object");*/
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = true,
+    isclose = true;
+
+console.log(isChecked && isclose);
